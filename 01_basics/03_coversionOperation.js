3cebc9da6efe04d41b3cb08eec0bd4cb9ec1acce
Ankur Kumar Singh <148796895+Akgitter@users.noreply.github.com>
2024-12-27 22:27:59 +0000

    practice 2

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,27 @@
+let score = "ankur"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" NaN
+// true=> 1; false =>0
+
+let isLoggedIn = "ankur"
+let booleanIsLogedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLogedIn);
+
+// 1=>true; 0=> false;
+// ""=>false
+// "name"=> true
+
+let someNumber = 21
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
