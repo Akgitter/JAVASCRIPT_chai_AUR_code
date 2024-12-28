0c046586405e345206aeb6a2e199efb0a00daef8
Ankur Kumar Singh <148796895+Akgitter@users.noreply.github.com>
2024-12-28 17:22:23 +0000

    practice 5

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+//Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigINt
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // Undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id== anotherId);
+
+// const bigNumber = 3423425551414123n
+
+
+
+//Reference (Non Primitive)
+
+// Array, Objects, Funtions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name :"hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world")    
+}
+
+
+
+
+
+
+
+
+
+
