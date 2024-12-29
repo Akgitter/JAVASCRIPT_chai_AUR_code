c6b8861a8be383527d051cf9be6ff9766f107166
Ankur Kumar Singh <148796895+Akgitter@users.noreply.github.com>
2024-12-29 20:54:13 +0000

    practice 6

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,8 +35,29 @@ const myFunction = function(){
 
 
 
+// Stack(Primitive), Heap (Non-Primitive)
 
+//
 
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "usergoggle.com",
+    upi: "user@ybl.com"
+}
+
+let userTwo = userOne
+
+userTwo.email ="hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
