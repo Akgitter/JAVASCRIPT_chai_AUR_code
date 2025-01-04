8aeff84a50197705e559fe8f0734cec5f39b82f1
Ankur Kumar Singh <148796895+Akgitter@users.noreply.github.com>
2025-01-04 20:27:20 +0000

    The_gAME_bEGINS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,3 +42,32 @@ function loginUserMessage(username = "lodu"){
 console.log(loginUserMessage("ankur"))
 
 
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is${anyobject.price}`);   
+}
+
+//handleObject(user) //For accesing data of object user
+handleObject( {
+    username: "sam",
+    price: 399
+}) //Directly giving the object without any name
+
+const myNewArray =[200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
