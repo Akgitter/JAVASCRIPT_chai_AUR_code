bb7c21f7a22de4b7ca2382bb1fd98b7171466330
Ankur Kumar Singh <148796895+Akgitter@users.noreply.github.com>
2025-01-05 18:50:13 +0000

    practice 13

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,45 @@ if (true) { //inside this called block scope
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username ="hitesh"
+
+    function two(){
+    const website = "youtube"
+    console.log(username);
+}
+// console.log(website);
+
+two()
+
+}
+
+// one()
+
+// if (true) {
+//     const username = "muskan"
+//     if (username === "muskan") {
+//         const website = "youtube"
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+
+// }
+
+// console.log(username);
+
+// +++++++++++++++++++++ interesting ++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+
+}
+
+
+
+addTwo(5)
+// also called epression
+const addTwo = function(num){
+    return num + 2
+}
+
